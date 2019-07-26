3991bbd594ced4d72f05f139afb5f2fefc3f91ee
Elbert Buchanan <e.b.memberships@outlook.com>
2019-07-26 15:27:32 -0500

    Elbert Bucvhanan Adding Swing conversions

---
+++ java_training/assingments/javafx_to_Java_Swing/Swing Scroll Pane Demo/src/swingscrollpanedemo/SwingSPMain.java
@@ -0,0 +1,44 @@
+package swingscrollpanedemo;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+
+
+public class SwingSPMain {
+	
+	public static void centerAndSizeJFrameOnScreen(JFrame frm, int width, int height) {
+		// set the size of the frame to half in width and height
+		frm.setSize(width, height);
+		// here's the part where the JFrame gets actually centered
+		frm.setLocationRelativeTo(null);
+}
+	
+	public static JFrame setFrameStuff(JFrame frm, int onExit, int width, int height) {
+		// this will center and size the JFrame on screen
+		// it will have a height and width of half the screen
+		centerAndSizeJFrameOnScreen(frm, width, height);
+		frm.setDefaultCloseOperation(onExit);
+		
+		return frm;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		SwingUtilities.invokeLater(new Runnable() {
+			ScrollStuff demo;
+			JFrame frm;
+		
+		public void run() {
+			demo = new ScrollStuff("swing Radio Button Demo");
+			frm = demo.getFrame();
+			frm = setFrameStuff(frm, JFrame.EXIT_ON_CLOSE, 400, 300);							
+			frm.setVisible(true);
+		}
+	});
+
+
+	}
+
+}
